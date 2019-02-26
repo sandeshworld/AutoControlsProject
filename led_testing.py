cb1a649219ec84dbf620a3c261b2e0514d9ef1d1
sandeshworld <sb3vf@virginia.edu>
2019-02-26 02:31:23 -0500

    Add files via upload
    Sending code to pin 3 to make sure that the Raspi is configured properly - LED testing

---
+++ led_testing.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3,GPIO.OUT)
+
+while True:
+    GPIO.output(3,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(3,GPIO.LOW)
+    time.sleep(1)
+
+
+    
