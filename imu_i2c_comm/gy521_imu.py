0cbb1b31a9c3b96f65a4f33100f66c346a16a256
sandeshworld <sb3vf@virginia.edu>
2019-03-04 00:07:11 -0500

    gyro works now but will have to improve object definition and calibrate it

--- imu_i2c_comm/gy521_imu.py
+++ imu_i2c_comm/gy521_imu.py
@@ -12,14 +12,19 @@ class gy521_imu:
 	def read(self):
 		with SMBusWrapper(1) as bus:
 			#configure by sending byte to 0x1C 
-			bus.write_byte_data(self.i2c_address,0x1C,0b11101000) #configuring the registers for the accelerometer
+			bus.write_byte_data(self.i2c_address,0x6B,0b00101000) #powermanagement register - turning it on
+			time.sleep(self.wait_cnt)
+			#bus.write_byte_data(self.i2c_address,0x1C,0b11101000) #configuring the registers for the accelerometer
 			time.sleep(self.wait_cnt)
 			while True:
-					b = bus.read_byte_data(self.i2c_address,0x3F) #each reading of x is sent in multiple operations
-					c = bus.read_byte_data(self.i2c_address,0x40)
-					print(b)
-					print(c)
-					time.sleep(self.wait_cnt)
+				z_ac_dat = bus.read_byte_data(self.i2c_address,0x3F) << 8 | bus.read_byte_data(self.i2c_address,0x40) #each reading of z (16 bits) is sent in multiple operations (2 - 8 bit messages)
+				x_ac_dat = bus.read_byte_data(self.i2c_address,0x3B) << 8 | bus.read_byte_data(self.i2c_address,0x3C) #each reading of z is sent in multiple operations
+				z_accel = (z_ac_dat)/16384.0 #apparently conversion rate - based on online resource
+				x_accel = (x_ac_dat)/16384.0 
+				print("z-accel: "+str(z_accel))
+				print("x-accel: "+str(x_accel))
+				
+				time.sleep(self.wait_cnt)
 
 
 
